8a4f6751ffcf5bce712a2564940e554f57541a4f
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-10-31 23:01:06 +0800

    Update taipei.py

--- taipei.py
+++ taipei.py
@@ -4,3 +4,6 @@ st.title("🎈 My taipei story")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.write(
+    "Let's test again!)."
+)
