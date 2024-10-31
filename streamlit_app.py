91ec5a6d3cccb6ee16af512896717f6aa8cb9f2d
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-10-31 23:23:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,14 @@ df = pd.DataFrame({
 })
 
 df
+dataframe1 = np.random.randn(10, 20)
+st.dataframe(dataframe1)
 dataframe = pd.DataFrame(
     np.random.randn(10, 20),
     columns=('col %d' % i for i in range(20)))
 
 st.dataframe(dataframe.style.highlight_max(axis=0))
+dataframe2 = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe2)
