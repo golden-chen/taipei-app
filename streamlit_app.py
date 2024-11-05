d4166b64693365c1afa049a4fe477565bd7fd193
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-05 17:54:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ if "logged_in" not in st.session_state:
 def login():
     if st.button("Log in"):
         pw=st.number_input('Please input password:')
-        if pw=='1234':
+        if pw=='1234.00':
             st.session_state.logged_in = True
             st.rerun()
 
