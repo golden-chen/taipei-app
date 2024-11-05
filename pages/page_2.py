4fb3f0fe127ca201f9356e843e2fcaac98a24c35
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-05 17:09:15 +0800

    Update page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -2,3 +2,27 @@ import streamlit as st
 
 st.markdown("# Page 2 ❄️")
 st.sidebar.markdown("# Page 2 ❄️")
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40],
+  'fruits':['apple','banana','cherry','orange']  
+})
+
+df
+dataframe1 = np.random.randn(10, 20)
+st.dataframe(dataframe1)
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
+dataframe2 = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe2)
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
