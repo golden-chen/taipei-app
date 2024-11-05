8e0b32af2e6592bde456a54eb644378ac57cb0eb
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-05 18:19:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ def login():
         st.session_state.pw=st.number_input('Please input password:',value=None)
         st.write("The current number is ", st.session_state.pw)       
         pw1=st.number_input('Please wait!',value=None)
-        if st.session_state.pw=='1234.0':
+        if st.session_state.pw=='1234.00':
             st.session_state.logged_in = True
             st.rerun()
 
