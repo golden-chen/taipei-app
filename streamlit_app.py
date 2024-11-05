bf7d96a26c04a88a0650eaf1305b23a7db16dcaf
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-05 18:21:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ def login():
         if st.session_state.pw=='1234.00':
             st.session_state.logged_in = True
             st.rerun()
-
+        else:st.write('password is wrong!')
 def logout():
     if st.button("Log out"):
         st.session_state.logged_in = False
