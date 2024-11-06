08ff2a0c2450085274565b894cd56cd5024bd51d
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-06 18:01:36 +0800

    Update page_3.py

--- pages/page_3.py
+++ pages/page_3.py
@@ -3,3 +3,17 @@ import foo
 st.write(foo.hello)
 st.markdown("# Page 3 🎉")
 st.sidebar.markdown("# Page 3 🎉")
+if 'button' not in st.session_state:
+    st.session_state.button = False
+
+def click_button():
+    st.session_state.button = not st.session_state.button
+
+st.button('Click me', on_click=click_button)
+
+if st.session_state.button:
+    # The message and nested widget will remain on the page
+    st.write('Button is on!')
+    st.slider('Select a value')
+else:
+    st.write('Button is off!')
