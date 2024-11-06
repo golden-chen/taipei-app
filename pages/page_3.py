d3d2f288dd94da723ffa9ea8aa8f493e63a095c0
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-06 19:57:21 +0800

    Update page_3.py

--- pages/page_3.py
+++ pages/page_3.py
@@ -30,6 +30,31 @@ with tab2:
     cols=st.columns(2)
     cols[0].header("A dog")
     cols[0].image("https://static.streamlit.io/examples/dog.jpg", width=100)
+    if 'stage' not in st.session_state:
+    st.session_state.stage = 0
+
+    def set_state(i):
+        st.session_state.stage = i
+    
+    if st.session_state.stage == 0:
+        st.button('Begin', on_click=set_state, args=[1])
+    
+    if st.session_state.stage >= 1:
+        name = st.text_input('Name', on_change=set_state, args=[2])
+    
+    if st.session_state.stage >= 2:
+        st.write(f'Hello {name}!')
+        color = st.selectbox(
+            'Pick a Color',
+            [None, 'red', 'orange', 'green', 'blue', 'violet'],
+            on_change=set_state, args=[3]
+        )
+        if color is None:
+            set_state(2)
+
+    if st.session_state.stage >= 3:
+        st.write(f':{color}[Thank you!]')
+        st.button('Start Over', on_click=set_state, args=[0])
 with tab3:
     cols=st.columns(2)
     cols[0].header("An owl")
