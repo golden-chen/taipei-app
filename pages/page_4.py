61f0b8489c44b6318798d3e89b6c5e3bef8941f3
golden-chen <33325840+golden-chen@users.noreply.github.com>
2024-11-09 05:43:38 +0800

    Update page_4.py

--- pages/page_4.py
+++ pages/page_4.py
@@ -1,4 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 import streamlit as st
 
 # Create the SQL connection to pets_db as specified in your secrets file.
